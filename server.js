8ee601dedbc22516d9f0e934d602ac2820c70af5
1willcobb <cobb.will@gmail.com>
2024-08-29 23:02:47 -0700

    yes

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const port = 3000;
 
 // Use path.resolve to get absolute paths directly
 app.get('/', (req, res) => {
+  console.log("accessing the root path");
   res.sendFile(path.resolve('index.html')); // Resolves to the current working directory + 'index.html'
 });
 
 app.get('/new', (req, res) => {
+  console.log("accessing the new path");
   res.sendFile(path.resolve('new.html')); // Resolves to the current working directory + 'new.html'
 });
 
