41d5a2d7840c15f936f413f837dd3af9885996a2
1willcobb <cobb.will@gmail.com>
2024-08-29 23:00:51 -0700

    basic layout

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
+// Use path.resolve to get absolute paths directly
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.resolve('index.html')); // Resolves to the current working directory + 'index.html'
 });
 
 app.get('/new', (req, res) => {
-  res.json({message:"sup dude"})
+  res.sendFile(path.resolve('new.html')); // Resolves to the current working directory + 'new.html'
 });
 
 app.listen(port, () => {
