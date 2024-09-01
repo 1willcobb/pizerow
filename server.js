0ecbf5ceca5d0753a13be9276e4a220af61a6446
1willcobb <cobb.will@gmail.com>
2024-08-31 20:20:39 -0700

    some cat data

--- server.js
+++ server.js
@@ -32,6 +32,22 @@ app.get('/start-time', (req, res) => {
   res.json({ startTime: serverStartTime });
 });
 
+app.get('/data', (req, res) => {
+  console.log("Accessing the data path");
+  const catsData = {
+    cats: [
+      { name: "Whiskers", age: 3, color: "gray" },
+      { name: "Mittens", age: 5, color: "white" },
+      { name: "Smokey", age: 2, color: "black" },
+      { name: "Luna", age: 4, color: "calico" },
+      { name: "Oliver", age: 1, color: "orange" }
+    ]
+  };
+
+  res.json(catsData);
+  res.status(200).json(catsData);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
